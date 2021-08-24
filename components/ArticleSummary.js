b38114bad47e09c7790cfb209e5a33345424cbe5
Joaquin Kunkel <jek537@nyu.edu>
2021-08-23 22:16:16 -0500

    Add space between ArticleSummary titles and their section's content

--- components/ArticleSummary.js
+++ components/ArticleSummary.js
@@ -4,33 +4,37 @@ import Block from "./Block";
 const ArticleSummary = ({problem, solution, impact, team}) => (
   <Grid container spacing={4}>
     <Grid item xs={6} md={3}>
-      <Typography gutterBottom variant="h6">
+      <Typography variant="h6">
         Problem
       </Typography>
+      <br />
       <Typography variant="body2" color="textSecondary">
         {problem}
       </Typography>
     </Grid>
     <Grid item xs={6} md={3}>
-      <Typography gutterBottom variant="h6">
+      <Typography variant="h6">
         Solution
       </Typography>
+      <br />
       <Typography variant="body2" color="textSecondary">
         {solution}
       </Typography>
     </Grid>
     <Grid item xs={6} md={3}>
-      <Typography gutterBottom variant="h6">
+      <Typography variant="h6">
         Impact
       </Typography>
+      <br />
       <Typography variant="body2" color="textSecondary">
         {impact}
       </Typography>
     </Grid>
     <Grid item xs={6} md={3}>
-      <Typography gutterBottom variant="h6">
+      <Typography variant="h6">
         Team
       </Typography>
+      <br />
       <Typography variant="body2" color="textSecondary">
         {team}
       </Typography>
